bbff84473e0f9c1fd0c62f386991f281d7563065
Delan Gono <30848798+SteiKnight@users.noreply.github.com>
2024-12-22 11:54:15 +0100

    modified: added todo tiles

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp/utils/todo_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,12 +12,17 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepPurple[100],
-      appBar: AppBar(
-        backgroundColor: Colors.deepPurple[600],
-        title: Text("TO DO"),
-        centerTitle: true,
-      ),
-    );
+        backgroundColor: Colors.deepPurple[100],
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple[600],
+          title: Text("TO DO",
+              style: TextStyle(
+                color: Colors.white,
+              )),
+          centerTitle: true,
+        ),
+        body: ListView(children: [
+          ToDoTile(),
+        ]));
   }
 }
